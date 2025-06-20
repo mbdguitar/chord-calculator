e18d3737d38e0a3f8a19e39f26ff4e8107a79929
MBarreto <mabd22998@gmail.com>
2025-06-20 14:24:51 -0400

    Added state management for active keys on Keyboard and KeyContainer components

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Octave from "./Octave.js"
 import keysGenerator from '../functions/keysGenerator.js';
 import styles from '../modules/Keyboard.module.css'
 
 function Keyboard() {
+    const [ activeKeys, setActiveKeys ] = useState([])
+
+    function updateActiveKeys(key) {
+        if (key.isActive) {
+            setActiveKeys((prev) => {
+                return [key, ...prev];
+            })
+        } else if (!key.isActive) {
+            setActiveKeys((prev) => {
+                return [...prev].filter((k) => k.id !== key.id);
+            })
+        }
+    }
+
     const keys = keysGenerator();
 
     return (
-        <div className={styles.keyboard_container}>
-            {keys.map((octave) => {
-                let octaveKeys = octave;
-                return <Octave octaveKeys={octaveKeys}/>
-            })}
-        </div>
+        <>
+            <div className={styles.keyboard_container}>
+                {keys.map((octave) => {
+                    let octaveKeys = octave;
+                    return <Octave octaveKeys={octaveKeys} updateActiveKeys={updateActiveKeys}/>
+                })}
+            </div>
+            <div>
+                {activeKeys.map((k) => {
+                    return <p>{k.name /*Code to remove after testing */}</p>
+                })}
+            </div>
+        </>
     );
 }
 
--- src/components/Octave.js
+++ src/components/Octave.js
@@ -2,20 +2,20 @@ import React from "react";
 import KeyContainer from '../containers/KeyContainer';
 import styles from '../modules/Octave.module.css'
 
-function Octave({ octaveKeys }) {
+function Octave({ octaveKeys, updateActiveKeys }) {
     return (
         <div className={styles.octave}>
             <div className={styles.top_row}>
                 {octaveKeys.map((key) => {
                     if (key.keyColor === 'black') {
-                        return <KeyContainer keyObj={key}/>
+                        return <KeyContainer keyObj={key} updateActiveKeys={updateActiveKeys}/>
                     }
                 })}
             </div>
             <div className={styles.bottom_row}>
                 {octaveKeys.map((key) => {
                     if (key.keyColor === 'white') {
-                        return <KeyContainer keyObj={key}/>
+                        return <KeyContainer keyObj={key} updateActiveKeys={updateActiveKeys}/>
                     }
                 })}
             </div>
