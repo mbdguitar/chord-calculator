32c3a8d85d8a9655f1bb6d88b4787c5886c815d6
MBarreto <mabd22998@gmail.com>
2025-06-23 23:59:04 -0400

    Added state for calculated intervals, and button component to calculate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
-import Keyboard from './Keyboard'
-import RootForm from './RootForm'
+import React, { useState, useEffect } from 'react';
+import { noteSorter } from '../functions/noteSorter';
+import getIntervals from '../functions/getIntervals';
+import Keyboard from './Keyboard';
+import RootForm from './RootForm';
+import CalculateButton from './CalculateButton';
 
 function Home() {
     const [ keys, setKeys ] = useState([]) 
     const [ root, setRoot ] = useState('')
+    const [ intervals, setIntervals ] = useState([])
+
+    useEffect(() => {
+        setKeys(noteSorter(keys))
+    }, [keys])
 
     function sendKeys(array) {
         setKeys(array);
@@ -14,10 +22,18 @@ function Home() {
         setRoot(target.value)
     }
 
+    function updateIntervals() {
+        setIntervals(getIntervals(keys, root));
+    }
+
     return (
         <>
             <Keyboard sendKeys={sendKeys}/>
             <RootForm getRoot={getRoot} keys={keys}/>
+            <CalculateButton updateIntervals={updateIntervals}/>
+            <div>
+                {intervals ? intervals.map((i) => <p>{i}</p>) : <p>please click on some notes</p>}
+            </div>
         </>
     );
 } 
