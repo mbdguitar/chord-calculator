c453f7514a3504ae0156ffba34c89681160bc85f
MBarreto <mabd22998@gmail.com>
2025-07-01 22:10:42 -0400

    Add code to re-render component on screen size change

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from "react";
+import useWindowWidth from "../hooks/useWindowWidth.js";
 import Octave from "./Octave.js"
 import keysGenerator from '../functions/keysGenerator.js';
 import styles from '../modules/Keyboard.module.css'
 
 function Keyboard({ sendKeys }) {
+    const [ keysToRender, setKeysToRender ] = useState(keysGenerator())
     const [ activeKeys, setActiveKeys ] = useState([])
+    const { width } = useWindowWidth();
 
     useEffect(() => {
         sendKeys(activeKeys)
     }, [activeKeys])
+
+    useEffect(() => {
+        setKeysToRender(keysGenerator());
+    }, [width])
 
     function updateActiveKeys(key) {
         if (key.isActive) {
@@ -22,12 +29,10 @@ function Keyboard({ sendKeys }) {
         }
     }
 
-    const keys = keysGenerator();
-
     return (
         <>
             <div className={styles.keyboard_container}>
-                {keys.map((octave) => {
+                {keysToRender.map((octave) => {
                     let octaveKeys = octave;
                     return <Octave octaveKeys={octaveKeys} updateActiveKeys={updateActiveKeys}/>
                 })}
