2fbafc8877872562ac71c4c4927e1b72a1d57942
MBarreto <mabd22998@gmail.com>
2025-07-02 20:11:17 -0400

    Remove 'use-strict' from code

--- src/functions/chordCalculator.js
+++ src/functions/chordCalculator.js
@@ -1,4 +1,3 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var chordData_1 = require("./chordData");
 function removeDuplicateIntervals(intervalsArray) {
--- src/functions/getIntervals.js
+++ src/functions/getIntervals.js
@@ -1,4 +1,3 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function getIntervals(notesArray, rootInput) {
     if (notesArray && rootInput) {
