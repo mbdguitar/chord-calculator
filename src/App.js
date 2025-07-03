71e049eebaf0b9c64fb72b86a5114f7348bb527b
MBarreto <mabd22998@gmail.com>
2025-07-02 20:10:28 -0400

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, NavLink, Router } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from "./components/About";
 import Root from "./components/Root";
