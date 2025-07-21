6f2a7ca657b797032183230afc5fa1227f225e03
MBarreto <mabd22998@gmail.com>
2025-07-21 14:32:52 -0400

    Add styling to menu buttons

--- src/components/InstrumentMenu.js
+++ src/components/InstrumentMenu.js
@@ -4,8 +4,8 @@ import styles from '../modules/InstrumentMenu.module.css';
 function InstrumentMenu({ handleInstrumentMenuButton }) {
     return (
         <div className={styles.button_container}>
-            <button className={styles.piano_button} onClick={handleInstrumentMenuButton} name={'Piano'}>Piano</button>
-            <button className={styles.guitar_button} onClick={handleInstrumentMenuButton} name={'Guitar'}>Guitar</button>
+            <button onClick={handleInstrumentMenuButton} name={'Piano'}>Piano</button>
+            <button onClick={handleInstrumentMenuButton} name={'Guitar'}>Guitar</button>
         </div>
     )
 }
