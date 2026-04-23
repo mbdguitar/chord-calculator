4ddaa8e172ab16148865d4de224a92cb96ea1bc1
MBarreto <mabd22998@gmail.com>
2026-04-23 19:08:28 +0200

    add netlify form support

--- src/components/About.js
+++ src/components/About.js
@@ -3,35 +3,53 @@ import { motion } from 'motion/react';
 import styles from '../modules/About.module.css';
 
 function About() {
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const formData = new FormData(e);
+
+            const response = await fetch('/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams(formData).toString()
+            });
+
+            if (!response.ok) throw new Error("homePage.contactSection.sentError");
+
+            (e).reset();
+            setMessage(t('homePage.contactSection.sentConfirmation'));
+        } catch (err) {
+            setMessage(t(err.message));
+        };
+    };
+
     return (
-        <motion.div 
+        <motion.div
             className={styles.about}
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1}}
-            transition={{transition: 0.1}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ transition: 0.1 }}
         >
             <section>
                 <h2>About Me</h2>
                 <p>
-                    Hey there! I'm Miguel, a music educator, performer, and front-end developer. 
-                    This app was built to help musicians, students, and producers quickly identify and understand chords played on a piano. 
-                    This project is the result of my desire to help my students understand chords and harmony in a way they have the freedom to 
+                    Hey there! I'm Miguel, a music educator, performer, and front-end developer.
+                    This app was built to help musicians, students, and producers quickly identify and understand chords played on a piano.
+                    This project is the result of my desire to help my students understand chords and harmony in a way they have the freedom to
                     experiment through an interactive interface.
                 </p>
             </section>
             <section>
                 <h2>How to use</h2>
                 <p>
-                    This app analyzes selected notes on the Keyboard and searches through a database to display the matching chord type. 
-                    It currently supports up to 250 different chord types, with inversions coming soon on a future update. Due to the complex
-                    nature of harmony and standard naming conventions in music theory, there is a chance the chord you're looking for is not
-                    able to be calculated (soon to be fixed).
+                    This app analyzes selected notes on the Keyboard and uses an algorithm to determine the name of the chord. Due to the complex nature of
+                    harmony. Just click on the piano to select the notes and hit calculate!
                 </p>
             </section>
             <section>
                 <h2>Coming Soon</h2>
                 <ul>
-                    <li>Full chord calculation algorithm</li>
                     <li>Support for chord inversions</li>
                     <li>Audio playback</li>
                 </ul>
@@ -40,14 +58,14 @@ function About() {
                 <h2>Contact</h2>
                 <p>Found some bugs or have any questions about the program? Let's get in touch!</p>
                 <div className={styles.contact_form_container}>
-                    <form>
+                    <form name='contact' data-netlify="true" onSubmit={(e) => handleSubmit(e)}>
                         <div className={styles.name}>
                             <label htmlFor='name'>NAME</label>
-                            <input id='name'type='text' name='name' placeholder="Enter your name" autocomplete></input>
+                            <input id='name' type='text' name='name' placeholder="Enter your name" autocomplete></input>
                         </div>
                         <div className={styles.email}>
                             <label htmlFor='email'>EMAIL</label>
-                            <input id='email'type='email' name='email' placeholder="Enter your email"></input>
+                            <input id='email' type='email' name='email' placeholder="Enter your email"></input>
                         </div>
                         <div className={styles.category}>
                             <label htmlFor='category'>CATEGORY</label>
@@ -61,9 +79,9 @@ function About() {
                         </div>
                         <div className={styles.message}>
                             <label htmlFor='message'>MESSAGE</label>
-                            <textarea id='message' name='message'placeholder="Enter your message"></textarea>
+                            <textarea id='message' name='message' placeholder="Enter your message"></textarea>
                         </div>
-                        <input type='submit' value='Submit' className={styles.about_submit_button}/>
+                        <input type='submit' value='Submit' className={styles.about_submit_button} />
                     </form>
                 </div>
             </section>
